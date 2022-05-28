33ae91ec19d676a4eea02e00c53365e6ba574014
Public Access <PublicAccess@nmpl.local>
2022-05-28 16:41:58 -0400

    Added new styles and adding functionality

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const mealForm = document.querySelector('form')
+let inputValue = document.querySelector('input').value
 
+const submitMealValue = (event) => {
+    let resultsHeader = document.querySelector('.results-header')
+    const resultsGrid = document.querySelector('.results-grid');
+
+    event.preventDefault()
+
+    resultsHeader.innerText = `Search results for ${inputValue}:`
+
+    inputValue = ''
+}
+
+mealForm.addEventListener('submit', submitMealValue)
