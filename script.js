7d5375aa136102f347f163cf19be8fca357517ab
Public Access <PublicAccess@nmpl.local>
2022-05-31 12:20:05 -0400

    Added functionality for the finder

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const mealForm = document.querySelector('form')
-let inputValue = document.querySelector('input').value
+const inputId = document.getElementById('category')
+const resultsHeader = document.querySelector('.results-header')
 
 const submitMealValue = (event) => {
-    let resultsHeader = document.querySelector('.results-header')
+    
     const resultsGrid = document.querySelector('.results-grid');
 
     event.preventDefault()
 
-    resultsHeader.innerText = `Search results for ${inputValue}:`
+    // if ()
+    resultsHeader.innerText = `Search results for '${inputId.value}':`
 
-    inputValue = ''
+    getInputData(inputId.value)
+    
+    fetchURL('https://www.themealdb.com/api/json/v1/1/list.php?i=list', inputId.value)
+    inputId.value = ''
+}
+
+const getInputData = data => data
+
+const fetchURL = (url, mealName) => {
+    fetch(url).then(response => response.json())
+    .then(data => {
+        console.log(data.meals)
+        console.log('Find index', data.meals[0].strIngredient.includes('Chicken'))
+        console.log('LOOK FOR', data.meals[0].strIngredient.includes(mealName))
+        
+        const getInfo = data.meals
+
+        for (let array = 0; array < data.meals.length; array++) {
+            if (getInfo[array].strIngredient.split(/\s+/).includes(mealName)) console.log('FOUND')
+        }
+        
+        for (let i = 1; i < getInfo.length; i++) {
+            const div = document.createElement('div')
+            const h2 = document.createElement('h3')
+            const img = document.createElement('img')
+
+            resultsHeader.appendChild(div)
+            div.append(h2, img)
+        }
+    })
 }
 
 mealForm.addEventListener('submit', submitMealValue)
